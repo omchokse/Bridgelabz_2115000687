39b0790f5365e9a5f2c5970c63fd4a2a35caf25c
Om Chokse <omchokse30@gmail.com>
2025-01-31 14:08:10 +0530

    codes added

---
+++ BookDetails.java
@@ -0,0 +1,25 @@
+class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("\nBook Details:");
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: $" + price);
+    }
+}
+
+public class BookDetails {
+    public static void main(String[] args) {
+        Book book1 = new Book("The Alchemist", "Paulo Coelho", 15.99);
+        book1.displayDetails();
+    }
+}
